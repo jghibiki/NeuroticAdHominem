2e30c8f70b70e483f2f154df6b5575d5031f52d0
jghibiki <jghibiki.games@gmail.com>
2016-03-23 16:41:02 -0500

    adds angular seed app

--- NeuroticAdHominem/app/Server.py
+++ NeuroticAdHominem/app/Server.py
@@ -4,13 +4,18 @@ from NeuroticAdHominem import eval
 
 def launch():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='', static_folder='app')
+    app.secret_key = "bGhmsER0KydumulGTRa2"
     api = Api(app)
 
-    class HelloWorld(Resource):
+    @app.route('/')
+    def root():
+        return app.send_static_file('index.html')
+
+    class EvalString(Resource):
         def get(self, test):
             return {"result": eval(test)}
 
-    api.add_resource(HelloWorld, "/<string:test>")
+    api.add_resource(EvalString, "/eval/<string:test>")
 
     app.run(debug=True, host="0.0.0.0")
